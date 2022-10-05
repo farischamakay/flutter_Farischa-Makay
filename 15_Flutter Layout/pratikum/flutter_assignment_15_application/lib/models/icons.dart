21a4c18d226cc3b6497797aeac42f8892097c391
Farischa Makay <57443923+farischamakay@users.noreply.github.com>
2022-10-05 23:41:12 +0700

    tes

---
+++ 15_Flutter Layout/pratikum/flutter_assignment_15_application/lib/models/icons.dart
@@ -0,0 +1,24 @@
+class GridIcons {
+  final String imgPath;
+
+  GridIcons({required this.imgPath});
+}
+
+final List<GridIcons> itemz = [
+  GridIcons(imgPath: 'assets/icon1.png'),
+  GridIcons(imgPath: 'assets/icon2.png'),
+  GridIcons(imgPath: 'assets/icon3.png'),
+  GridIcons(imgPath: 'assets/icon4.png'),
+  GridIcons(imgPath: 'assets/icon5.png'),
+  GridIcons(imgPath: 'assets/icon6.png'),
+  GridIcons(imgPath: 'assets/icon7.png'),
+  GridIcons(imgPath: 'assets/icon8.png'),
+  GridIcons(imgPath: 'assets/icon9.png'),
+  GridIcons(imgPath: 'assets/icon10.png'),
+  GridIcons(imgPath: 'assets/icon11.png'),
+  GridIcons(imgPath: 'assets/icon12.png'),
+  GridIcons(imgPath: 'assets/icon13.png'),
+  GridIcons(imgPath: 'assets/icon14.png'),
+  GridIcons(imgPath: 'assets/icon15.png'),
+  GridIcons(imgPath: 'assets/icon16.png'),
+];
