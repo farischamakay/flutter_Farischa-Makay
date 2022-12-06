e764aae8cb431dd9c086c0d2154907b4da261f27
Farischa Makay <57443923+farischamakay@users.noreply.github.com>
2022-12-06 15:21:16 +0700

    upload pratikum assignment

---
+++ 25_Introduction REST API - JSON Serialization or Deserialization/pratikum/flutter_pratikum_api/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Kampus Merdeka\\Alterra-20220824T050235Z-001\\Alterra\\flutter_Farischa-Makay\\25_Introduction REST API - JSON Serialization or Deserialization\\pratikum\\flutter_pratikum_api" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=D:\\Kampus Merdeka\\Alterra-20220824T050235Z-001\\Alterra\\flutter_Farischa-Makay\\25_Introduction REST API - JSON Serialization or Deserialization\\pratikum\\flutter_pratikum_api"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Kampus Merdeka\\Alterra-20220824T050235Z-001\\Alterra\\flutter_Farischa-Makay\\25_Introduction REST API - JSON Serialization or Deserialization\\pratikum\\flutter_pratikum_api\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Kampus Merdeka\\Alterra-20220824T050235Z-001\\Alterra\\flutter_Farischa-Makay\\25_Introduction REST API - JSON Serialization or Deserialization\\pratikum\\flutter_pratikum_api"
+  "FLUTTER_TARGET=D:\\Kampus Merdeka\\Alterra-20220824T050235Z-001\\Alterra\\flutter_Farischa-Makay\\25_Introduction REST API - JSON Serialization or Deserialization\\pratikum\\flutter_pratikum_api\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Kampus Merdeka\\Alterra-20220824T050235Z-001\\Alterra\\flutter_Farischa-Makay\\25_Introduction REST API - JSON Serialization or Deserialization\\pratikum\\flutter_pratikum_api\\.dart_tool\\package_config.json"
+)
